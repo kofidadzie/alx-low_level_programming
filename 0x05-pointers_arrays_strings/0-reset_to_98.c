33500ff8c8c05b10b7f271f4497d662a346ed66c
kofidadzie <danielkofidadzie@gmail.com>
2023-02-27 11:29:10 -0800

    98 Battery

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - Fxn moves pointer to an in as parameter
+ * updates the value it points to as 98
+ * @n: input
+ * Return: n
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
