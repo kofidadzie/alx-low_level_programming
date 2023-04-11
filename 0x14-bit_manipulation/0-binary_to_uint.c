0bb563208cae901460db0f6ea4bee784bdad7fca
kofidadzie <danielkofidadzie@gmail.com>
2023-04-10 17:10:40 -0700

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,14 +9,15 @@
 unsigned int binary_to_uint(cont char *b)
 {
 	int a;
-	unsigned int num = 0;
+	unsigned int dec_val = 0;
 
 	if (!b)
 		return (0);
 	for (a = 0; b[a]; a++)
-	{
+	{{
 		if (b[a] < '0' || b[a] > '1')
 			return (0);
-		num = 2 * num + (b[a] - '0');
+	 }
+		dec_val = 2 * dec_val + (b[a] - '0');
 	}
 }
